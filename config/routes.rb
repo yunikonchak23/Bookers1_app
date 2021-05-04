0dc777bde6546a6f7f03aa0d2ce46d12e64ba72b
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-04 13:20:13 +0000

    [Add}bookers1_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'books/new'
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
+  root :to => 'homes#top'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show',as:'book'
+  post 'books' => 'books#create'
+  get 'books/ :id' => 'books#show', as:'book'
+  get 'books/ :id/ edit' => 'books#edit', as:'edit_book'
+  patch 'books/ :id' => 'books#update', as:'update_book'
+  put 'books/ :id' => 'books#update'
+  delete 'books/ :id' => 'books#destroy'
+  
 end
