1478e22b0198bd99afe13a96ac012b935c99b10f
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-04 13:31:39 +0000

    [Add]bookers1-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,28 +2,41 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new(book_params)
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     #1.新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     #2.データベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     #3.Show画面へリダイレクト
-    redirect_to book_path(book.id)
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
   private
   #ストロングパラメータ
   def book_params
