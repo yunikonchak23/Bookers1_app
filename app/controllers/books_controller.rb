8b137289224b5b88bcd4ba4cf2caf39b16bc002f
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-04 19:28:12 +0000

    [Add]bookers1-edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   private
   #ストロングパラメータ
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 
 end
